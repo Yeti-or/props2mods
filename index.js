41aad29cb345f5ec2fc7f17a00fa5a6bede30492
awinogradov <winogradovaa@gmail.com>
2016-07-18 15:11:24 +0300

    add reactify

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 'use strict';
 
-exports.reactify = function reactify(config) {
+exports.declareMods = function declareMods(config) {
   let mods = {};
   config.states.concat(Object.keys(config.mods)).forEach(mod => {
     mods[mod] = true;
   });
+
   return mods;
 }
 
-exports.bemify = function bemify(props, declaredMods) {
+exports.reactify = function reactify(bemjson, declaredMods) {
+  let props = {};
+  if(bemjson.mods) {
+    props = Object.assign(props, bemjson.mods);
+    delete bemjson.mods;
+  }
+
+  return Object.assign(props, bemjson);
+}
+
+exports.bemifyMods = function bemifyMods(props, declaredMods) {
   let mods = {};
   Object.keys(props).forEach(prop => {
     if(declaredMods[prop]) mods[prop] = props[prop];
   });
+  
   return mods;
 }
